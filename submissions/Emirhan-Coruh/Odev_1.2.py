37f33d9bb2a62498f925e4236eeaeb79b8581b8a
Emirhan Çoruh <coruhemirhan85@gmail.com>
2026-03-05 17:19:46 +0000

    Ödev 1.2

---
+++ submissions/Emirhan-Coruh/Odev_1.2.py
@@ -0,0 +1,15 @@
+
+ogrenci_notlari = {
+    "Vindicta":[44, 63, 52],
+    "Vyper":[86, 73, 78],
+    "Shiv":[38, 67, 56]
+}
+
+ogrenciler_ = {"Vindicta","Vyper","Shiv"}
+okul = ("Gazi University", 1926)
+
+ikinci_not = ogrenci_notlari["Shiv"][1]
+print("Öğrencinin 2. notu: ", ikinci_not)
+
+ogrenci_sayisi = len(ogrenci_notlari)
+print("Öğrenci sayısı: ", ogrenci_sayisi)
